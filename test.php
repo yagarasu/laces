728ef9bf680bc55293499f7cc09c81b67c1bcfb4
Alexys Hegmann <alexys.hegmann@gmail.com>
2015-03-18 17:08:09 +0000

    Context finished. Uses Regular Expressions to understand where in the array tree save the data.

--- test.php
+++ test.php
@@ -1,28 +1,15 @@
 <?php
 
-	require 'laces/Laces.class.php';
+	require 'laces/Context.class.php';
+	
+	$c = new Context();
+	
+	$c->set('$foo:bar', 'foooooo');
+	$c->set('COOONST', 'const const');
+	$c->set('#myId', array('asd','asd'));
 
-	$l = new Laces(array(
-		'foo'	=> 'FOO<hr>OOOO',
-		'bar'	=> array(
-			'baz' => 'BAAAZZZ',
-			'sql' => "SELEC<b>T</b> ''-- \x00 \n dasd \x00 \n \r \\ \x1a "
-		),
-		'attrs' => 'y me dijo "woooo"...'
-	));
+	var_dump($c->exists('#myId'));
 
-	$l->render('
-		{{{ LacesTemplate author="yagarasu" language="es_MX" }}}
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore impedit rem placeat architecto, deserunt nihil quis tempora facere. Aliquam non numquam deleniti iusto dolore accusantium, veniam, tenetur recusandae obcaecati modi!
-		~{{ $foo | html }}~
-
-		~{{ $bar:baz | html }}~
-
-		~{{ $bar:sql | mysql | html }}~
-
-		"~{{ $attrs | attr }}~"
-
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, similique, temporibus. Pariatur explicabo labore velit dolorum temporibus in tempora, nostrum dolorem at error nisi dolore fuga numquam officiis unde veniam!
-	');
+	//var_dump($c->getRawArray());
 
 ?>
